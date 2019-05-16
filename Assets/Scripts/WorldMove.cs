b2fbf4f4ac98847b04ebe820158b3d13a468af1f
KotomiNagata <bomb1997free@gmail.com>
2019-05-16 09:56:03 +0900

    初アップデート

---
+++ Assets/Scripts/WorldMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMove : MonoBehaviour
+{
+
+    public int walkSpeed;
+    public int rotationSpeed;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        // 上キー：前進
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(new Vector3(walkSpeed, 0, 0) * Time.deltaTime, Space.World);
+        }
+        // 下キー：後退
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(new Vector3(-walkSpeed, 0, 0) * Time.deltaTime, Space.World);
+        }
+        // 右キー：反時計回り
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, -rotationSpeed, 0) * Time.deltaTime, Space.World);
+        }
+        // 左キー：時計回り
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.deltaTime, Space.World);
+        }
+    }
+}
