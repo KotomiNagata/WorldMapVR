9a8151218d346ba6bbcec52b179797ae98f25566
KotomiNagata <bomb1997free@gmail.com>
2019-06-28 16:53:17 +0900

    消える前に保存！

---
+++ Assets/Scripts/AnimationByArray.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationByArray : MonoBehaviour
+{
+    public GameObject[] array; // これが配列
+    public bool looping = true;
+    public float AnimationWaitTime = 0.01f;
+    public int activeIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 子供のゲームオブジェクトの数分だけ配列に入れます
+        array = new GameObject[gameObject.transform.childCount];
+        for (int i = 0; i < gameObject.transform.childCount; i ++ ){
+            array[i] = gameObject.transform.GetChild(i).gameObject;
+        }
+
+        // 最初の一つだけ表示させます
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i == activeIndex) {
+                array[i].SetActive(true);
+            }else{
+                array[i].SetActive(false);
+            }
+        }
+        StartCoroutine("ChangeActive");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator ChangeActive()
+    {
+        // Activeの切り替え
+        while ( looping){
+            yield return new WaitForSeconds(AnimationWaitTime);
+            array[activeIndex].SetActive(false);
+            if ( activeIndex == array.Length -1 ){
+                activeIndex = 0;
+            }else{
+                activeIndex++;
+            }
+            array[activeIndex].SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Animation_Sea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_Sea : MonoBehaviour
+{
+
+    public float AnimationWaitTime = 0.01f;
+    public GameObject nextAnimationObject;
+
+    void Start()
+    {
+        StartCoroutine("CloneMake");
+    }
+
+    /*
+    void Update()
+    {
+
+    }*/
+
+    private IEnumerator CloneMake()
+    {
+        yield return new WaitForSeconds(AnimationWaitTime);
+        Instantiate(nextAnimationObject);
+        Destroy(this.gameObject);
+    }
+
+}
