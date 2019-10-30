404ee09d20a312b3a177ec5f8535fa23401d7058
KotomiNagata <bomb1997free@gmail.com>
2019-10-30 17:26:26 +0900

    apply

--- Assets/Scripts/Title/Title_Map.cs
+++ Assets/Scripts/Title/Title_Map.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Title_Map : MonoBehaviour
 {
+    FadeObject fade;
+    public GameObject fadeInObj;
+    public string sceneName;
+    bool creatObj = true;
 
     void Update()
     {
         transform.Rotate(new Vector3(0, 0.1f, 0));
+
+        if(Input.GetKey("joystick button 17") && creatObj)
+        {
+            Instantiate(fadeInObj);
+            fade = FindObjectOfType<FadeObject>();
+            fade.nextSceneName = sceneName;
+            creatObj = false;
+        }
     }
 }
