2a8ee2bb353d5acb7a7d6b716b2c9b4a3ca0d5cb
KotomiNagata <bomb1997free@gmail.com>
2019-11-12 10:45:10 +0900

    apply

---
+++ Assets/Scripts/GamePlay/QuizTextSetting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizTextSetting : MonoBehaviour
+{
+    float posY = 0.2f;
+    float rotX = 60f;
+    float size = 0.2f;
+
+    void Start()
+    {
+        this.transform.position = new Vector3(0, posY, 0);
+        this.transform.localEulerAngles = new Vector3(rotX, 0, 0);
+        this.transform.localScale = new Vector3(size, size, size);
+    }
+}
